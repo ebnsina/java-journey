5d24ca2dd059b78db80c0291da69ae62bfea31d6
Ebn Sina <ebnsina.me@gmail.com>
2022-08-17 18:16:05 +0600

    refactor to methods

--- Main.java
+++ Main.java
@@ -1,11 +1,46 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        greetUser("Ebn Sina");
+        int principle = (int) readNumber("Principle: ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest: ", 1, 30);
+        byte years = (byte) readNumber("Period (Years): ", 1, 30);
+
+        double mortgage = calculateMortgage(principle, annualInterest, years);
+
+        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + formattedMortgage);
+        System.out.println(formattedMortgage);
     }
 
-    public static void greetUser(String name) {
-        System.out.println("Hello, " + name + "!");
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.println(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a valid value between " + min + " & " + max);
+        }
+
+        return value;
+    }
+
+
+    public static double calculateMortgage(int principle, float annualInterest, byte years) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double mortgage = principle
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
     }
 
 }
