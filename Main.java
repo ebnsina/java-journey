1fafc90161b9f3497600858471b05536563bf8db
Ebn Sina <ebnsina.me@gmail.com>
2022-08-17 15:14:24 +0600

    mortgage calculator start

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
@@ -57,9 +58,34 @@ public class Main {
         System.out.println(b);
 
         // Get input from user
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("You name " + name);
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter your name: ");
+        // String name = scanner.nextLine();
+        // System.out.println("You name " + name);
+
+        // Mortgage Calculator
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner mortgageScanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principle = mortgageScanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = mortgageScanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (in Years): ");
+        byte years = mortgageScanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principle
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + formattedMortgage);
     }
 }
